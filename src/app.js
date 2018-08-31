9ec982ff3e54b20e149060c54ad2fd49e9813018
Rodrigo <cherry199662@gmail.com>
2018-08-31 18:32:03 -0500

    Test Hot Module

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 
 const app = new Vue({
   data: {
-    hello: 'Hello',
+    hello: 'Hello 3212',
   },
+  template: '<div id="app"><h1>{{hello}}</h1></div>'
 });
 
 export {app};
