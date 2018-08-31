221e58a8c652020b29096b188c2bfc8f95184fe4
Rodrigo <cherry199662@gmail.com>
2018-08-31 18:19:00 -0500

    Add dev-middleware

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const path = require('path');
 const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
 })();
+
+app.use('/dist', express.static(path.resolve(__dirname, './dist')));
+
+require('./build/dev-server')(app);
 app.get('*', (req, res) => {
   res.write(indexHTML);
   res.end();
--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log(`app`)
+import Vue from 'vue';
+
+const app = new Vue({
+  data: {
+    hello: 'Hello',
+  },
+});
+
+export {app};
