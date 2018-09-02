acd23ebd6c7fc4044f4b594ac59c47cc1f12f77e
Rodrigo <cherry199662@gmail.com>
2018-09-01 19:18:20 -0500

    Separate Category and Post

--- src/theme/AppFooter.vue
+++ src/theme/AppFooter.vue
@@ -1,5 +1,13 @@
 <template>
-    
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                Follow us on
+                <a href=""
+                   target="_blank">Twitter</a>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <nav class="nav has-shadow">
+        <div class="container">
+            <a href="/">
+                <img src="http://bit.ly/vue-img"
+                     alt="Vue SPA" />
+            </a>
+        </div>
+    </nav>
 </template>
 
 <script>
