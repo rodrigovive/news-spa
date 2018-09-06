d98832fb00afafb1fad98ecbfaa3925df13b23b3
Rodrigo <cherry199662@gmail.com>
2018-09-06 12:49:38 -0500

    webpack server setup

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack')
-
 const clientConfig = require('./webpack.client.config')
+const serverConfig = require('./webpack.server.config')
+const MFS = require('memory-fs')
+const path = require('path')
 
 module.exports = function setupDevServer (app, onUpdate) {
   clientConfig.entry.app = [
@@ -20,4 +22,12 @@ module.exports = function setupDevServer (app, onUpdate) {
     })
   )
   app.use(require('webpack-hot-middleware')(clientCompiler))
+
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, 'server/main.js')
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, () => {
+    onUpdate(mfs.readFileSync(outputPath, 'utf-8'))
+  })
 }
