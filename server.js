309452ed7ddcf38817a3acffaa72d3aa75e35de1
Rodrigo <cherry199662@gmail.com>
2018-09-26 11:27:53 -0500

    Server-side Bundle Callback

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
-
+const { createBundleRenderer } = require('vue-server-renderer')
+let renderer
 const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
 })()
 
 app.use('/dist', express.static(path.resolve(__dirname, './dist')))
 
-require('./build/dev-server')(app)
+require('./build/dev-server')(app, bundle => {
+  renderer = createBundleRenderer(bundle)
+})
 app.get('*', (req, res) => {
-  res.write(indexHTML)
-  res.end()
+  renderer.renderToString({url: req.url}, (err, html) => {
+    if (err) {
+      return res.status(500).send('Error server')
+    }
+    html = indexHTML.replace('{{ APP }}', html)
+    res.write(html)
+    res.end()
+  })
 })
 
 const port = process.env.PORT || 3000
